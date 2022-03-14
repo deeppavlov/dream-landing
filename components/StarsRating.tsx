c9f2f58082831976e54e2eaa2ab201bcbfd76f14
mablin7 <magyarb0201@gmail.com>
2022-03-14 11:48:38 +0300

    fix: split out StarsRating

---
+++ components/StarsRating.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useCallback, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./starsrating.module.css";
+
+const StarsRating: FC<{ rating: number; setRating: (r: number) => void }> = ({
+  rating,
+  setRating,
+}) => {
+  const color = useCallback(
+    (idx: number) => ({ color: idx <= rating ? "#ffd93a" : "gray" }),
+    [rating]
+  );
+
+  return (
+    <div className={styles["stars"]}>
+      {Array(5)
+        .fill(0)
+        .map((_, idx) => (
+          <FontAwesomeIcon
+            key={idx}
+            icon={faStar}
+            size="2x"
+            style={color(idx)}
+            onClick={() => setRating(idx)}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default StarsRating;
