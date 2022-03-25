481f58759163f28be040bec99ff6eafced30e339
mablin7 <magyarb0201@gmail.com>
2022-03-25 16:54:27 +0300

    feat: add stars animation

--- components/StarsRating.tsx
+++ components/StarsRating.tsx
@@ -3,13 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./starsrating.module.css";
+import stylesAnim from "./starsanimation.module.css";
 import { usePopup } from "./Popup";
 
 const StarsRating: FC<{
   rating: number;
   setRating: (r: number) => void;
   showFeedbackLink?: boolean;
-}> = ({ rating, setRating, showFeedbackLink = false }) => {
+  animate?: boolean;
+}> = ({ rating, setRating, animate = false, showFeedbackLink = false }) => {
   const { show } = usePopup();
 
   return (
@@ -24,7 +26,11 @@ const StarsRating: FC<{
               icon={faStar}
               size="2x"
               color={idx <= rating ? "#ffd93a" : "gray"}
-              onClick={() => setRating(idx)}
+              onClick={() => (console.log("rated", idx), setRating(idx))}
+              className={animate ? stylesAnim["star-jump"] : ""}
+              style={{
+                animationDelay: `${idx * 0.3}s`,
+              }}
             />
           ))}
       </div>
