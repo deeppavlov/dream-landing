54112f990eea2d81080e5026564cbefc1831e6ec
mablin7 <magyarb0201@gmail.com>
2022-03-27 17:15:06 +0300

    fix: update viewport meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("../mocks/browser");
@@ -7,7 +8,17 @@ if (process.env.NODE_ENV === "development") {
 }
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
