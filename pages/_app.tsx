8c29d6eefcae584d049a14ae15931d37edecc593
mablin7 <magyarb0201@gmail.com>
2022-03-27 17:50:08 +0300

    fix: safari autozoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          content="device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
         />
       </Head>
       <Component {...pageProps} />
