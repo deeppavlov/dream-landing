4fc3a5242226e09676f9590553bc944f03b6b3d3
mablin7 <magyarb0201@gmail.com>
2022-03-30 21:06:05 +0300

    misc: disable most mocks

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -4,20 +4,20 @@ import { nanoid } from "nanoid";
 const API_URL = "https://7019.lnsigo.mipt.ru/";
 
 const handlers = [
-  rest.post(API_URL, (req, res, ctx) => {
-    return res(
-      ctx.json({
-        dialog_id: "dialog_id",
-        utt_id: nanoid(),
-        user_id: (req.body as Record<string, string>).user_id,
-        response: "Hey there!",
-        active_skill: "skill",
-      })
-    );
-  }),
+  // rest.post(API_URL, (req, res, ctx) => {
+  //   return res(
+  //     ctx.json({
+  //       dialog_id: "dialog_id",
+  //       utt_id: nanoid(),
+  //       user_id: (req.body as Record<string, string>).user_id,
+  //       response: "Hey there!",
+  //       active_skill: "skill",
+  //     })
+  //   );
+  // }),
 
   rest.post(API_URL + "rating/utterance", (req, res, ctx) => res()),
-  rest.post(API_URL + "rating/dialog", (req, res, ctx) => res()),
+  // rest.post(API_URL + "rating/dialog", (req, res, ctx) => res()),
 ];
 
 export const worker = typeof window !== "undefined" && setupWorker(...handlers);
