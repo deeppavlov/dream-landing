4be760d6bb7952019d7c3bd030cdcb541af17df1
mablin7 <magyarb0201@gmail.com>
2022-05-13 13:43:27 +0200

    feat: action btn slide

--- components/ActionBtn.tsx
+++ components/ActionBtn.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useLayoutEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import cn from "classnames";
 
 import styles from "./actionbtn.module.css";
 
@@ -16,6 +17,65 @@ const ActionBtn: FC<{ onClick?: () => void; icon: IconDefinition }> = ({
       </div>
       {children}
     </button>
+  );
+};
+
+export const ActionBtnGroup: FC<{
+  children:
+    | React.ReactElement<typeof ActionBtn>[]
+    | React.ReactElement<typeof ActionBtn>;
+}> = ({ children }) => {
+  return <div className={styles["group"]}>{children}</div>;
+};
+
+export const ActionBtnSlide: FC<{
+  activeIdx?: number;
+  toggleOnClick?: boolean;
+  disabled?: boolean;
+  children: (
+    | React.ReactElement<typeof ActionBtnGroup>
+    | React.ReactElement<typeof ActionBtn>
+  )[];
+}> = ({ activeIdx = 0, toggleOnClick = false, disabled = false, children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const overflowRef = useRef<HTMLDivElement>(null);
+
+  // Hack to prevent running in SSR
+  if (typeof window !== "undefined") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useLayoutEffect(() => {
+      if (!overflowRef.current || !wrapperRef.current) return;
+      // Set wrapper height
+      wrapperRef.current.style.height = `${overflowRef.current.scrollHeight}px`;
+    }, []);
+  }
+
+  const [idxOverride, setIdxOverride] = useState<number | null>(null);
+  const onClick = () => {
+    if (!toggleOnClick) return;
+    setIdxOverride((idx) => ((idx ?? activeIdx) + 1) % children.length);
+  };
+
+  return (
+    <div
+      ref={wrapperRef}
+      className={cn(
+        styles["slide-wrapper"],
+        disabled && styles["slide-disabled"]
+      )}
+      onClick={onClick}
+    >
+      <div
+        ref={overflowRef}
+        className={styles["slide-overflow"]}
+        style={{
+          width: `${children.length * 100}%`,
+          left: `${-(idxOverride ?? activeIdx) * 100}%`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
