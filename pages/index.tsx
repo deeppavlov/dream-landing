f4369e7be689d5ea71b998a3075b4d22bcfcdbf3
mablin7 <magyarb0201@gmail.com>
2022-05-18 15:42:58 +0200

    fix: close sidebar on reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ const Chat: NextPage = () => {
                 `${messages.slice(-1)?.[0]?.utteranceId ?? ""}`,
                 getChatPic
               )}
-              onReset={reset}
+              onReset={() => (setSidebarOpen(false), reset())}
             ></Sidebar>
           </div>
 
