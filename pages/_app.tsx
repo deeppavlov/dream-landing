7b5b9c0fd94419d518bc270b1e8c2716344f3980
mablin7 <magyarb0201@gmail.com>
2022-05-18 15:53:55 +0200

    feat: add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,22 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       {process.env.NODE_ENV !== "development" ? (
         <>
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(88822179, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    trackHash:true
+              });
+            `}
+          </Script>
+
           <Script id="google-analytics" strategy="afterInteractive">
             {`
               window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
